9df69cd874eefca47fb8e776629b4d505a8b8e07
João Vicente Souto <joaovicentesouto@gmail.com>
2018-09-07 14:10:11 -0300

    gui

---
+++ apps/main.cpp
@@ -0,0 +1,17 @@
+
+#include <ai/Minimax.hpp>
+
+void print_board(ai::board_type board)
+{
+    for (int i = 0; i < 15; ++i) {
+        for (int j = 0; j < 15; ++j) {
+
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
