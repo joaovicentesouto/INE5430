634ad0f48dc046ac5c65f2de175657527a1ea19e
bonotto <bruno_mcl@hotmail.com.br>
2018-11-22 19:44:41 -0200

    Neural network finalization

--- neural_network.m
+++ neural_network.m
@@ -34,41 +34,39 @@ for i = 1:20
     xlabel(target_string{target(perm(i))+1,2});
 end
 
-%codifica a entrada usando o esquema onehot, target i eh igual a 1 na linha i+1
+% Codificacao onehot da entrada para treinamento
 for i=1:size(input,1)
     class_index = target(i,1)+1;
     TrainingOutput(class_index, i) = 1;
 end
 
-% onehot para testes
+% Codificacao onehot da entrada para teste
 for i=1:size(input_test,1)
     class_index = target_test(i,1)+1;
     TestOutput(class_index, i) = 1;
 end
 
-% normalizacao entre -1 e 1
+% Normalizacao dos pixels com valores entre 0 e 255 para -1 e 1
 TrainingData = (((input*2)/255) -1)';
 TestData = (((input_test*2)/255) -1)';
 
-%cria rede neural
-x=80
-net = newff(TrainingData, TrainingOutput, x, {'tansig', 'tansig'}, 'traingdx');
+% Criacao da rede neural
+neuronsAmount = 80
+net = newff(TrainingData, TrainingOutput, neuronsAmount, {'tansig', 'tansig'}, 'traingdx');
 net.trainParam.epochs=500;
 net.trainParam.goal=0.01;
-%net.trainParam.lr=0.1;          %taxa de aprendizado
-net.trainParam.max_fail=8;     %epocas com erro sobre validacao
+net.trainParam.max_fail=6;
+net.divideParam.trainRatio=0.8;
+net.divideParam.valRatio=0.2;
+net.divideParam.testRatio=0;
 
-net.divideParam.trainRatio=0.85; % Tudo para treino
-net.divideParam.valRatio=0.15;     % Nada para valid
-net.divideParam.testRatio=0;    % Nada para teste
-
-%Treina a rede.
+% Treinamento da rede neural
 net=train(net, TrainingData, TrainingOutput);
 
-%Simula a rede para os dados do treinamento.
+% Simulacao da rede neural com os dados do treinamento
 Y=sim(net, TrainingData);
 
-%Satura a saida da rede para os mesmos valores de saida dos exemplos de treinamento.
+% Saturacao da saida da rede neural para os mesmos valores de saida dos exemplos de treinamento
 for j=1:size(Y,2)
 	max = -1;
     index = -1;
@@ -81,7 +79,7 @@ for j=1:size(Y,2)
     YClass(j,1) = index-1;
 end
 
-%Onehot yclass
+% Codificacao onehot da saida para treinamento
 for i=1:size(YClass,1)
     class_index = YClass(i,1)+1;
     TrainingResponse(class_index, i) = 1;
@@ -89,10 +87,10 @@ end
 
 clear YClass;
 
-%Simula a rede para os dados de teste.
+% Simulacao da rede neural para os dados de teste
 Y=sim(net, TestData);
 
-%Satura a saida da rede para os mesmos valores de saida dos exemplos de treinamento.
+% Saturacao da saida da rede neural para os mesmos valores de saida dos exemplos de teste
 for j=1:size(Y,2)
 	max = -1;
     index = -1;
@@ -105,7 +103,7 @@ for j=1:size(Y,2)
     YClass(j,1) = index-1;
 end
 
-%Onehot yclass
+% Codificacao onehot da saida para teste
 for i=1:size(YClass,1)
     class_index = YClass(i,1)+1;
     TestResponse(class_index, i) = 1;
