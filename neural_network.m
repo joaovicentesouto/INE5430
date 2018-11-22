1d7372b24c0ac9354d392f86a84e874069fa8af1
bonotto <bruno_mcl@hotmail.com.br>
2018-11-22 15:26:47 -0200

    Empirical test of neural network model to classify clothes

--- neural_network.m
+++ neural_network.m
@@ -1,17 +1,18 @@
 clear all
 close all
 
-Data=importfile('fashion-mnist_train.csv',2,inf);
+Data=csvread('fashion-mnist_train.csv',1,0);
 input = Data(:,2:end);
 target= Data(:,1);
 
-Data=importfile('fashion-mnist_test.csv',2,inf);
+Data=csvread('fashion-mnist_train.csv',1,0);
 input_test = Data(:,2:end);
 target_test= Data(:,1);
 
 clearvars Data;
 
-target_string={0 'T-shirt/top';
+target_string={
+0 'T-shirt/top';
 1 'Trouser';
 2 'Pullover';
 3 'Dress';
@@ -33,55 +34,79 @@ for i = 1:20
     xlabel(target_string{target(perm(i))+1,2});
 end
 
-% A partir deste ponto os dados já estão importados para o ambiente.
-% Entretanto você tem de atentar que:
-%     - as entradas e saídas para uma rede neural no MATLAB estão nas linhas
-% da matriz;
-%     - se o problema é de classificação é desejável codificar as
-% saídas categóricas usando o esquema "onehot" onde cada neurônio da camada
-% de saída codifica uma dada categoria.
-
 %codifica a entrada usando o esquema onehot, target i eh igual a 1 na linha i+1
-TrainingOutput = onehot(input, target);
+for i=1:size(input,1)
+    class_index = target(i,1)+1;
+    TrainingOutput(class_index, i) = 1;
+end
 
 % onehot para testes
-TestOutput = onehot(input_test, target);
+for i=1:size(input_test,1)
+    class_index = target_test(i,1)+1;
+    TestOutput(class_index, i) = 1;
+end
 
 % normalizacao entre -1 e 1
 TrainingData = (((input*2)/255) -1)';
 TestData = (((input_test*2)/255) -1)';
 
-cont=1;
-
 %cria rede neural
-x=115
+x=80
 net = newff(TrainingData, TrainingOutput, x, {'tansig', 'tansig'}, 'traingdx');
 net.trainParam.epochs=500;
 net.trainParam.goal=0.01;
+%net.trainParam.lr=0.1;          %taxa de aprendizado
+net.trainParam.max_fail=8;     %epocas com erro sobre validacao
 
 net.divideParam.trainRatio=0.85; % Tudo para treino
 net.divideParam.valRatio=0.15;     % Nada para valid
 net.divideParam.testRatio=0;    % Nada para teste
 
-%Treine a rede.
+%Treina a rede.
 net=train(net, TrainingData, TrainingOutput);
 
-%Simule a rede para os dados do treinamento.
+%Simula a rede para os dados do treinamento.
 Y=sim(net, TrainingData);
 
-%Sature a saida da rede para os mesmos valores de saida dos exemplos de treinamento.
-YClass = sature(Y)
+%Satura a saida da rede para os mesmos valores de saida dos exemplos de treinamento.
+for j=1:size(Y,2)
+	max = -1;
+    index = -1;
+    for i=1:size(Y,1)
+    	if Y(i,j) > max
+        	max = Y(i,j);
+            index = i;
+        end
+	end
+    YClass(j,1) = index-1;
+end
 
 %Onehot yclass
-a = onehot(YClass, YClass);
+for i=1:size(YClass,1)
+    class_index = YClass(i,1)+1;
+    TrainingResponse(class_index, i) = 1;
+end
 
 clear YClass;
 
-%Simule  a rede para os dados de teste.
+%Simula a rede para os dados de teste.
 Y=sim(net, TestData);
 
-%Sature a saida da rede para os mesmos valores de saida dos exemplos de treinamento.
-YClass = sature(Y)
+%Satura a saida da rede para os mesmos valores de saida dos exemplos de treinamento.
+for j=1:size(Y,2)
+	max = -1;
+    index = -1;
+    for i=1:size(Y,1)
+    	if Y(i,j) > max
+        	max = Y(i,j);
+            index = i;
+        end
+	end
+    YClass(j,1) = index-1;
+end
 
 %Onehot yclass
-b = onehot(YClass, YClass);
+for i=1:size(YClass,1)
+    class_index = YClass(i,1)+1;
+    TestResponse(class_index, i) = 1;
+end
